127c49b2150ce5d701750a6e92726fca0ca3b100
Gibran Herrera <gibranhl2@gmail.com>
2021-12-13 23:00:34 -0600

    Add unrevised version of the repository

---
+++ repository/mongo.go
@@ -0,0 +1,59 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/GibranHL0/devblog/connection"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoRepository struct {
+	Db connection.Database
+}
+
+func (mr *MongoRepository) GetArticle(id primitive.ObjectID) (
+	*mongo.SingleResult, error) {
+
+	filter := bson.M{"_id": id}
+	ctx, cancel := context.WithTimeout(context.Background(), mr.Db.Timing)
+	defer cancel()
+
+	result := mr.Db.Collection.FindOne(ctx, filter)
+
+	if err := result.Err(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (mr *MongoRepository) GetHomeView(skip int64, limit int64) (
+	*mongo.Cursor, error) {
+
+	filter := bson.M{}
+	ctx, cancel := context.WithTimeout(context.Background(), mr.Db.Timing)
+	defer cancel()
+
+	articlesView := mr.Db.Collection.Database().Collection("HomeArticles")
+
+	findOptions := options.FindOptions{
+		Skip: &skip,
+		Limit: &limit,
+		Sort: bson.D{{"date", 1}},
+	}
+
+	cursor, err := articlesView.Find(
+		ctx,
+		filter,
+		&findOptions,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return cursor, nil
+}
