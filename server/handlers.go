083ea6072d5c89fb87956d9f519d4a6a4db5d5fc
Gibran Herrera <gibranhl2@gmail.com>
2021-12-15 23:55:21 -0600

    Add Article page

--- server/handlers.go
+++ server/handlers.go
@@ -3,6 +3,11 @@ package server
 import (
 	"net/http"
 	"text/template"
+
+	"github.com/GibranHL0/devblog/connection"
+	"github.com/GibranHL0/devblog/models"
+	"github.com/GibranHL0/devblog/repository"
+	"github.com/GibranHL0/devblog/services"
 )
 
 func aboutHandler(templates *template.Template) http.HandlerFunc {
@@ -17,8 +22,30 @@ func contactHandler(templates *template.Template) http.HandlerFunc {
 	}
 }
 
-func newsletterHandler(templates *template.Template) http.HandlerFunc{
+func newsletterHandler(templates *template.Template) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		executeTemplate(templates, "newsletter.html", nil, rw)
+	}
+}
+
+func articleHandler(templates *template.Template, db *connection.Database) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+
+		articleId := r.URL.Query().Get("id")
+		if articleId == "" {
+			executeNotFound(rw)
+			return
+		}
+
+		article := models.Article{}
+		mr := repository.MongoRepository{Db: db}
+
+		err := services.GetArticle(articleId, mr, &article)
+		if err != nil {
+			executeNotFound(rw)
+			return
+		}
+
+		executeTemplate(templates, "article.html", article, rw)
 	}
 }
--- server/router.go
+++ server/router.go
@@ -5,30 +5,34 @@ import (
 	"text/template"
 
 	"github.com/GibranHL0/devblog/configuration"
+	"github.com/GibranHL0/devblog/connection"
 )
 
 func initializeHandlers(
 	mux *http.ServeMux,
-	config *configuration.Configuration,
-	templates *template.Template) {
+	db *connection.Database,
+	templates *template.Template,
+	config *configuration.Configuration) {
 	
 	// Creates a http handler that will work as a File Server
 	fileServer := createFileServer(config.FileServerPath)
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/article", articleHandler(templates, db))
 	mux.HandleFunc("/about", aboutHandler(templates))
 	mux.HandleFunc("/contact", contactHandler(templates))
 	mux.HandleFunc("/suscribe", newsletterHandler(templates))
 }
 
 func StartMux(
+	db *connection.Database,
 	config *configuration.Configuration,
 	templates *template.Template) *http.ServeMux {
 	// Starts a fresh Mux
 	mux := http.NewServeMux()
 
 	// Routes all the handlers with their respective pattern
-	initializeHandlers(mux, config, templates)
+	initializeHandlers(mux, db, templates, config)
 
 	return mux
 }
